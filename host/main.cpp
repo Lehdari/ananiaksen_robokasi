449f67789a94525f62545a84c264f6744c299318
akuantti <akuantti@hotmail.com>
2021-04-25 18:45:21 +0300

    Added serialib communication

--- host/main.cpp
+++ host/main.cpp
@@ -13,7 +13,63 @@
 #include <unistd.h>
 #endif
 
+#if defined (_WIN32) || defined(_WIN64)
+    #define SERIAL_PORT "COM1"
+#endif
+#if defined (__linux__) || defined(__APPLE__)
+    #define SERIAL_PORT "/dev/ttyS6"
+#endif
 
+int main(int argc, char const *argv[])
+{
+    // Serial object
+    serialib serial;
+
+    // Connection to serial port
+    char errorOpening = serial.openDevice(SERIAL_PORT, 9600);
+
+    // If connection fails, return the error code otherwise, display a success message
+    if (errorOpening==1)
+        printf ("Successful connection to %s\n",SERIAL_PORT);
+    else {
+        printf("Couldn't establish connection to %s (%d)\n",SERIAL_PORT, (int)errorOpening);
+        return -1;
+    }
+
+    // Defines initial state
+    int state[6] = {90, 90, 90, 90, 90, 120};
+
+    // Send and receive char buffers
+    char send[512] = {0};
+    unsigned char receive[512] = {0};
+
+    while(true)
+    {
+        for (int i=0; i<512; ++i) {
+            send[i] = 0;
+            receive[i] = 0;
+        }
+
+        int joint;
+        printf("Lol gief joint\n");
+        std::cin >> joint;
+        printf("Lol gief ankle\n");
+        std::cin >> state[joint];
+
+        std::stringstream ss;
+        ss << "A" << state[0] << "B" << state[1] << "C" << state[2] << "D" << state[3] << "E" << state[4] << "F" << state[5];
+
+        serial.writeString(ss.str().c_str());
+        printf("sent: %s\n", ss.str().c_str());
+
+        serial.flushReceiver();
+    }
+    return 0;
+}
+
+
+
+#if 0
 int main()
 {
     int cport_nr=5;        /* /dev/ttyS0 (COM1 on windows) */
@@ -71,3 +127,4 @@ int main()
 
     return(0);
 }
+#endif
